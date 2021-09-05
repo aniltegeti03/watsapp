9d60222e2cc7f6005b957dd04dc2b27dabdfec2f
aniltegeti03 <aniltegeti@gmail.com>
2021-09-05 22:55:07 +0530

    this is developed by anil

---
+++ src/main/java/watsapp/TimeResource.java
@@ -0,0 +1,16 @@
+package watsapp;
+
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/time")
+@Produces("text/plain")
+public class TimeResource {
+	@GET
+	public String getTime() {
+		return new Date().toString();
+	}
+}
---
+++ src/test/java/watsapp/TimeResourceTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013 ${company}.
+ */
+package watsapp;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import javax.inject.Inject;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.atteo.moonshine.Moonshine;
+import org.atteo.moonshine.tests.MoonshineConfiguration;
+import org.atteo.moonshine.tests.MoonshineTest;
+import org.atteo.moonshine.webserver.WebServerAddress;
+import org.junit.Test;
+
+import com.google.common.io.Resources;
+
+@MoonshineConfiguration(autoConfiguration = true)
+public class TimeResourceTest extends MoonshineTest {
+	@Inject
+	private WebServerAddress address;
+
+	@Test
+	public void shouldReturnTime() throws IOException, MalformedURLException {
+		System.out.println(Resources.toString(new URL("http://localhost:" + address.getPort() + "/time"), StandardCharsets.UTF_8));
+	}
+}
